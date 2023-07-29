198244de91eb5a42fddfbf880497025431977d63
gk7734 <gamekiller7734@naver.com>
2023-07-30 00:56:12 +0900

    로그인 구현 성공

--- backend/app/src/routes/home/home.ctrl.js
+++ backend/app/src/routes/home/home.ctrl.js
@@ -1,14 +1,39 @@
 "use strict";
 
-const home = (req, res) =>{
-    res.render("home/index");
-}
+const users = {
+    id: ["gk", "gamekiller", "sgk"],
+    pwd: ["1234", "1234", "123"],
+};
 
-const login = (req, res) =>{
-    res.render("home/login");
-}
+const output = {
+    home: (req, res) =>{
+        res.render("home/index");
+    },
+    login: (req, res) =>{
+        res.render("home/login")
+    }
+};
+
+const process = {
+    login: (req, res) =>{
+        const id = req.body.id,
+         pwd = req.body.pwd;
+      if (users.id.includes(id)) {
+          const idx = users.id.indexOf(id);
+          if (users.pwd[idx] === pwd) {
+              return res.json({
+                  success: true,
+              });
+          }
+      }
+      return res.json({
+          success: false,
+          msg: "로그인 실패하셨습니다.",
+      });
+    },
+};
 
 module.exports = {
-    home,
-    login,
-}
+    output,
+    process,
+};
